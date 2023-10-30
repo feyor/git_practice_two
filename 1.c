37b6abe4c2656a58c05b6a483c3da2503de863e3
Pavel Domarev <dmrpv1@yandex.ru>
2023-10-30 21:16:13 +0300

    Second_Commit

---
+++ 1.c
@@ -0,0 +1,18 @@
+//Пользователь вводит количество секунд, прошедших с начала суток. Вывести , прошедших с начала последней минуты.
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int get_info;
+    int integer;
+    int get_off_integer;
+
+    printf("Введите количество секунд, прошедших с начала суток: ");
+    scanf("%d", &get_info);
+    integer=get_info/60;
+    get_off_integer=get_info-(integer*60);
+
+    printf("Количество секунд, прошедших с начала последней минуты: %4d\n", get_off_integer);
+}
